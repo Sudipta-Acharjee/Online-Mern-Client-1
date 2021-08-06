604b1d7b1ce2244cd4f3799051590c8e88e5820a
Sudipta acharjee <sudiptoacharjee4@gmail.com>
2021-08-06 13:49:50 +0600

    add admin form

--- src/Component/MainPart/AddAdmin/AddAdmin.js
+++ src/Component/MainPart/AddAdmin/AddAdmin.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Sidebar from '../Sidebar/Sidebar';
 
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
 const AddAdmin = () => {
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = data => console.log(data);
     return (
         <div className="row">
             <div className="col-md-3">
                 <Sidebar></Sidebar>
             </div>
             <div className="col-md-9">
-                <h1>Hello make admin</h1>
+                <form style={customStyles} className="p-5" onSubmit={handleSubmit(onSubmit)}>
+                    <input type="text" class="form-control" placeholder="Enter Your Email" {...register("email")} required />
+                    {errors?.email && <span className="text-danger">This field is required</span>}
+                    <button type="submit" className="btn btn-primary mt-3">Add Admin</button>
+                </form>
             </div>
         </div>
     );
